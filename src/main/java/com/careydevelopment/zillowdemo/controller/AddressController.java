9269e2b3072232a4fbb80df0097755c58c2c7e09
bcarey <bcarey@LAPTOP-9IJC4N3L>
2017-06-06 18:12:34 -0400

    formatting changes

--- src/main/java/com/careydevelopment/zillowdemo/controller/AddressController.java
+++ src/main/java/com/careydevelopment/zillowdemo/controller/AddressController.java
@@ -16,44 +16,44 @@ import com.careydevelopment.zillowdemo.util.ZillowApiHelper;
 @Controller
 public class AddressController {
 
-	//starting page for zillow api demo
-	@RequestMapping(value = "/enterAddress", method=RequestMethod.GET)
-	public String enterAddress(Model model) {
-		//instantiate an empty address object
-		Address address = new Address();
-		
-		//put the object in the model
-		model.addAttribute("address", address);
-		
-		//get out
-		return "enterAddress";
-	}
-
-	
-	@RequestMapping(value = "/enterAddress", method=RequestMethod.POST)
-	public String formSubmit(@Valid Address address, BindingResult bindingResult, Model model) {
-		//check for errors
-		if (bindingResult.hasErrors()) {
+    //starting page for zillow api demo
+    @RequestMapping(value = "/enterAddress", method=RequestMethod.GET)
+    public String enterAddress(Model model) {
+        //instantiate an empty address object
+        Address address = new Address();
+    	
+        //put the object in the model
+        model.addAttribute("address", address);
+    	
+        //get out
+        return "enterAddress";
+    }
+    
+    
+    @RequestMapping(value = "/enterAddress", method=RequestMethod.POST)
+    public String formSubmit(@Valid Address address, BindingResult bindingResult, Model model) {
+        //check for errors
+        if (bindingResult.hasErrors()) {
             return "enterAddress";
         }
-		
-		//get the search results object
-		ZillowSearchResults results = ZillowApiHelper.getSearchResults(address);
-		
-		//put it in the model
-		model.addAttribute("results", results);
-		
-		//add the address to the model as well
-		model.addAttribute("address", address);
-		
-		//get out
-		return "showZillowResults";
-	}
-		
-	
-	//redirect to demo if user hits the root
-	@RequestMapping("/")
-	public String home(Model model) {
-		return "redirect:enterAddress";
-	}
+    	
+        //get the search results object
+        ZillowSearchResults results = ZillowApiHelper.getSearchResults(address);
+    	
+        //put it in the model
+        model.addAttribute("results", results);
+    	
+        //add the address to the model as well
+        model.addAttribute("address", address);
+    	
+        //get out
+        return "showZillowResults";
+    }
+    	
+    
+    //redirect to demo if user hits the root
+    @RequestMapping("/")
+    public String home(Model model) {
+        return "redirect:enterAddress";
+    }
 }
